e35c2c9b9b17be4f641ea6b6a6e8b9d07f683290
sorenchu <antonioostras@gmail.com>
2017-07-10 20:41:31 +0200

    Extracting styles from template to css file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,90 +1,43 @@
 import { Component } from '@angular/core';
 
-export class Animal {
-    name: string;
+export class Family {
     id: number;
-    family: Family;
+    name: string;
+    diet: string;
+    noise: string;
+    ability: string;
+}
+
+export class Animal {
+    id: number;
+    name: string;
+    age: number;
 }
 
 @Component({
   selector: 'my-app',
   template: `<h1>{{name}}</h1>
-            <h2>{{animal.name}}</h2>
-            <div><label>id: </label>{{animal.id}}</div>
-            <div>
-              <label>name: </label>
-              <input [(ngModel)]="animal.name" placeholder="name">
-            </div>
-
-            <h2>My animals</h2>
             <ul class="animals">
-                <li *ngFor="let animal of animals" (click)="onSelect(animal)>
+                <li *ngFor="let animal of animals" >
                     <span class="badge">{{animal.id}}</span> {{animal.name}}
                 </li>
-            </ul>`
-            ,
-            styles: [`
-              .selected {
-                background-color: #CFD8DC !important;
-                color: white;
-              }
-              .animals {
-                margin: 0 0 2em 0;
-                list-style-type: none;
-                padding: 0;
-                width: 15em;
-              }
-              .animals li {
-                cursor: pointer;
-                position: relative;
-                left: 0;
-                background-color: #EEE;
-                margin: .5em;
-                padding: .3em 0;
-                height: 1.6em;
-                border-radius: 4px;
-              }
-              .animals li.selected:hover {
-                background-color: #BBD8DC !important;
-                color: white;
-              }
-              .animals li:hover {
-                color: #607D8B;
-                background-color: #DDD;
-                left: .1em;
-              }
-              .animals .text {
-                position: relative;
-                top: -3px;
-              }
-              .animals .badge {
-                display: inline-block;
-                font-size: small;
-                color: white;
-                padding: 0.8em 0.7em 0 0.7em;
-                background-color: #607D8B;
-                line-height: 1em;
-                position: relative;
-                left: -1px;
-                top: -4px;
-                height: 1.8em;
-                margin-right: .8em;
-                border-radius: 4px 0 0 4px;
-              }
-            `]
+            </ul>`,
 })
 
 export class AppComponent  {
-  name = 'Animals';
-  animal: Animal = {
-    name: 'Toby',
-    id: 1
-  };
-  animals: Animals[] = [
-    {id: '11', name: 'Bobby'},
-    {id: '12', name: 'Toby'},
-    {id: '13', name: 'Speaker'},
-    {id: '14', name: 'Zipper'},
-    {id: '15', name: 'Walker'},
-  ];
+    name = 'Animals';
+    animals: Animals[] = [
+      {id: '11', name: 'Bobby'},
+      {id: '12', name: 'Toby'},
+      {id: '13', name: 'Speaker'},
+      {id: '14', name: 'Zipper'},
+      {id: '15', name: 'Walker'},
+    ];
+    family: Family = {
+      id: 1,
+      name: 'Dogs',
+      diet: 'Meat',
+      noise: 'guau',
+      ability: 'playing with balls'
+    }
 }
