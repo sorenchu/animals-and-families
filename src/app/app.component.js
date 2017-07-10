de9b693d9015b9a0d19388c520a1ae1f01aaab27
sorenchu <antonioostras@gmail.com>
2017-07-10 20:24:31 +0200

    List of animals

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -19,13 +19,21 @@ var AppComponent = (function () {
             name: 'Toby',
             id: 1
         };
+        this.animals = [
+            { id: '11', name: 'Bobby' },
+            { id: '12', name: 'Toby' },
+            { id: '13', name: 'Speaker' },
+            { id: '14', name: 'Zipper' },
+            { id: '15', name: 'Walker' },
+        ];
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<h1>{{name}}</h1>\n            <h2>{{animal.name}}</h2>\n            <div><label>id: </label>{{animal.id}}</div>\n            <div>\n              <label>name: </label>\n              <input [(ngModel)]=\"animal.name\" placeholder=\"name\">\n            </div>",
+        template: "<h1>{{name}}</h1>\n            <h2>{{animal.name}}</h2>\n            <div><label>id: </label>{{animal.id}}</div>\n            <div>\n              <label>name: </label>\n              <input [(ngModel)]=\"animal.name\" placeholder=\"name\">\n            </div>\n\n            <h2>My animals</h2>\n            <ul class=\"animals\">\n                <li *ngFor=\"let animal of animals\" (click)=\"onSelect(animal)>\n                    <span class=\"badge\">{{animal.id}}</span> {{animal.name}}\n                </li>\n            </ul>",
+        styles: ["\n              .selected {\n                background-color: #CFD8DC !important;\n                color: white;\n              }\n              .animals {\n                margin: 0 0 2em 0;\n                list-style-type: none;\n                padding: 0;\n                width: 15em;\n              }\n              .animals li {\n                cursor: pointer;\n                position: relative;\n                left: 0;\n                background-color: #EEE;\n                margin: .5em;\n                padding: .3em 0;\n                height: 1.6em;\n                border-radius: 4px;\n              }\n              .animals li.selected:hover {\n                background-color: #BBD8DC !important;\n                color: white;\n              }\n              .animals li:hover {\n                color: #607D8B;\n                background-color: #DDD;\n                left: .1em;\n              }\n              .animals .text {\n                position: relative;\n                top: -3px;\n              }\n              .animals .badge {\n                display: inline-block;\n                font-size: small;\n                color: white;\n                padding: 0.8em 0.7em 0 0.7em;\n                background-color: #607D8B;\n                line-height: 1em;\n                position: relative;\n                left: -1px;\n                top: -4px;\n                height: 1.8em;\n                margin-right: .8em;\n                border-radius: 4px 0 0 4px;\n              }\n            "]
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 export class Animal {
     name: string;
     id: number;
+    family: Family;
 }
 
 @Component({
@@ -13,7 +14,64 @@ export class Animal {
             <div>
               <label>name: </label>
               <input [(ngModel)]="animal.name" placeholder="name">
-            </div>`,
+            </div>
+
+            <h2>My animals</h2>
+            <ul class="animals">
+                <li *ngFor="let animal of animals" (click)="onSelect(animal)>
+                    <span class="badge">{{animal.id}}</span> {{animal.name}}
+                </li>
+            </ul>`
+            ,
+            styles: [`
+              .selected {
+                background-color: #CFD8DC !important;
+                color: white;
+              }
+              .animals {
+                margin: 0 0 2em 0;
+                list-style-type: none;
+                padding: 0;
+                width: 15em;
+              }
+              .animals li {
+                cursor: pointer;
+                position: relative;
+                left: 0;
+                background-color: #EEE;
+                margin: .5em;
+                padding: .3em 0;
+                height: 1.6em;
+                border-radius: 4px;
+              }
+              .animals li.selected:hover {
+                background-color: #BBD8DC !important;
+                color: white;
+              }
+              .animals li:hover {
+                color: #607D8B;
+                background-color: #DDD;
+                left: .1em;
+              }
+              .animals .text {
+                position: relative;
+                top: -3px;
+              }
+              .animals .badge {
+                display: inline-block;
+                font-size: small;
+                color: white;
+                padding: 0.8em 0.7em 0 0.7em;
+                background-color: #607D8B;
+                line-height: 1em;
+                position: relative;
+                left: -1px;
+                top: -4px;
+                height: 1.8em;
+                margin-right: .8em;
+                border-radius: 4px 0 0 4px;
+              }
+            `]
 })
 
 export class AppComponent  {
@@ -22,4 +80,11 @@ export class AppComponent  {
     name: 'Toby',
     id: 1
   };
+  animals: Animals[] = [
+    {id: '11', name: 'Bobby'},
+    {id: '12', name: 'Toby'},
+    {id: '13', name: 'Speaker'},
+    {id: '14', name: 'Zipper'},
+    {id: '15', name: 'Walker'},
+  ];
 }
