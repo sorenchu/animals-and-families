9b627ac494cff1c445743496e49509555ec25486
sorenchu <antonioostras@gmail.com>
2017-07-10 19:27:06 +0200

    First form with angularJs

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -6,16 +6,26 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var core_1 = require("@angular/core");
+var Animal = (function () {
+    function Animal() {
+    }
+    return Animal;
+}());
+exports.Animal = Animal;
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Animals and Families';
+        this.name = 'Animals';
+        this.animal = {
+            name: 'Toby',
+            id: 1
+        };
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<h1>Hello {{name}}</h1>",
+        template: "<h1>{{name}}</h1>\n            <h2>{{animal.name}}</h2>\n            <div><label>id: </label>{{animal.id}}</div>\n            <div>\n              <label>name: </label>\n              <input [(ngModel)]=\"animal.name\" placeholder=\"name\">\n            </div>",
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,25 @@
 import { Component } from '@angular/core';
 
+export class Animal {
+    name: string;
+    id: number;
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>{{name}}</h1>
+            <h2>{{animal.name}}</h2>
+            <div><label>id: </label>{{animal.id}}</div>
+            <div>
+              <label>name: </label>
+              <input [(ngModel)]="animal.name" placeholder="name">
+            </div>`,
 })
-export class AppComponent  { name = 'Animals and Families'; }
+
+export class AppComponent  {
+  name = 'Animals';
+  animal: Animal = {
+    name: 'Toby',
+    id: 1
+  };
+}
