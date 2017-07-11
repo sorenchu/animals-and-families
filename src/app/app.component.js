c91519aae55f5662b46670cc086d69e89153550f
sorenchu <antonioostras@gmail.com>
2017-07-11 19:21:54 +0200

    Playing around with js and methods

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -18,19 +18,38 @@ var AppComponent = (function () {
                 ability: 'Sing' },
         ];
         this.animals = [
-            { id: '11', name: 'Bobby' },
-            { id: '12', name: 'Toby' },
-            { id: '13', name: 'Speaker' },
-            { id: '14', name: 'Zipper' },
-            { id: '15', name: 'Walker' },
+            { id: '11', name: 'Bobby', family: this.families[0] },
+            { id: '12', name: 'Toby', family: this.families[1] },
+            { id: '13', name: 'Speaker', family: this.families[2] },
+            { id: '14', name: 'Zipper', family: this.families[0] },
+            { id: '15', name: 'Walker', family: this.families[0] },
         ];
     }
+    AppComponent.prototype.onSelect = function (family) {
+        this.selectedFamily = family;
+        this.getAnimals(family, this.animals);
+    };
+    AppComponent.prototype.count = function (family) {
+        return family.length;
+    };
+    AppComponent.prototype.getAnimals = function (family, animals) {
+        var arrayOfAnimals = [];
+        animals.forEach(function (a) {
+            if (a.family === family) {
+                arrayOfAnimals.push(a);
+                console.log(a);
+                console.log(arrayOfAnimals);
+            }
+        });
+        console.log(arrayOfAnimals);
+        this.animalsOfFamily = arrayOfAnimals;
+    };
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<h1>{{globalList}}</h1>\n            <ul class=\"animals\">\n                <li *ngFor=\"let family of families\" >\n                    <span class=\"badge\">{{family.id}}</span> {{family.name}}\n                    eat {{family.diet}} and there are {{families.length}}\n                </li>\n            </ul>",
+        template: "<h1>{{globalList}}</h1>\n            <ul class=\"animals\">\n                <li *ngFor=\"let family of families\"\n                [class.selected]=\"family === selectedFamily\"\n                (click)=\"onSelect(family)\">\n                    <span class=\"badge\">{{family.id}}</span> {{family.name}}\n                    eat {{family.diet}} and there are {{families.length}}\n                </li>\n            </ul>\n            <div *ngIf=\"selectedFamily\">\n                <h2>{{selectedFamily.name}}</h2>\n                <li *ngFor=\"let animal of animalsOfFamily\">\n                    <span class=\"badge\">{{animal.id}}</span> {{animal.name}}\n                </li>\n            </div>",
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,19 @@ import { Animal } from './animal';
   selector: 'my-app',
   template: `<h1>{{globalList}}</h1>
             <ul class="animals">
-                <li *ngFor="let family of families" >
+                <li *ngFor="let family of families"
+                [class.selected]="family === selectedFamily"
+                (click)="onSelect(family)">
                     <span class="badge">{{family.id}}</span> {{family.name}}
                     eat {{family.diet}} and there are {{families.length}}
                 </li>
-            </ul>`,
+            </ul>
+            <div *ngIf="selectedFamily">
+                <h2>{{selectedFamily.name}}</h2>
+                <li *ngFor="let animal of animalsOfFamily">
+                    <span class="badge">{{animal.id}}</span> {{animal.name}}
+                </li>
+            </div>`,
 })
 
 export class AppComponent  {
@@ -24,10 +32,34 @@ export class AppComponent  {
           ability: 'Sing'},
     ];
     animals: Animal[] = [
-      {id: '11', name: 'Bobby'},
-      {id: '12', name: 'Toby'},
-      {id: '13', name: 'Speaker'},
-      {id: '14', name: 'Zipper'},
-      {id: '15', name: 'Walker'},
+      {id: '11', name: 'Bobby', family: this.families[0]},
+      {id: '12', name: 'Toby', family: this.families[1]},
+      {id: '13', name: 'Speaker', family: this.families[2]},
+      {id: '14', name: 'Zipper', family: this.families[0]},
+      {id: '15', name: 'Walker', family: this.families[0]},
     ];
+    selectedFamily: GlobalFamily;
+    animalsOfFamily: Animal[];
+
+    onSelect(family: GlobalFamily): void {
+        this.selectedFamily = family;
+        this.getAnimals(family, this.animals);
+    }
+
+    count(family: GlobalFamily): int {
+        return family.length;
+    }
+
+    getAnimals(family: GlobalFamily, animals: Animal[]): void {
+        var arrayOfAnimals = [];
+        animals.forEach(function(a) {
+            if (a.family === family) {
+                arrayOfAnimals.push(a);
+                console.log(a);
+                console.log(arrayOfAnimals);
+            }
+        });
+        console.log(arrayOfAnimals);
+        this.animalsOfFamily = arrayOfAnimals;
+    }
 }
