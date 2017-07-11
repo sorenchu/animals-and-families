a449c51b3b3afb491212a1cf12e33b15d0c6aefa
sorenchu <antonioostras@gmail.com>
2017-07-11 20:47:21 +0200

    Separating global list from specific list
    TODO: known issue. It is not being loaded the whole list in the first load
    of the site

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -6,6 +6,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var core_1 = require("@angular/core");
+var core_2 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
         this.globalList = 'Global List';
@@ -17,39 +18,46 @@ var AppComponent = (function () {
             { id: 3, name: 'Birds', diet: 'Seed', noise: 'Sing',
                 ability: 'Sing' },
         ];
+        this.specificList = 'Specific List';
         this.animals = [
-            { id: '11', name: 'Bobby', family: this.families[0] },
-            { id: '12', name: 'Toby', family: this.families[1] },
-            { id: '13', name: 'Speaker', family: this.families[2] },
-            { id: '14', name: 'Zipper', family: this.families[0] },
-            { id: '15', name: 'Walker', family: this.families[0] },
+            { id: '11', name: 'Bobby', age: 6, family: this.families[0] },
+            { id: '12', name: 'Toby', age: 24, family: this.families[2] },
+            { id: '13', name: 'Speaker', age: 2, family: this.families[2] },
+            { id: '14', name: 'Zipper', age: 11, family: this.families[1] },
+            { id: '15', name: 'Walker', age: 8, family: this.families[0] },
         ];
     }
-    AppComponent.prototype.onSelect = function (family) {
-        this.selectedFamily = family;
-        this.getAnimals(family, this.animals);
+    AppComponent.prototype.showGlobalList = function (flag) {
+        this.globalOrSpecific = flag;
     };
-    AppComponent.prototype.count = function (family) {
-        return family.length;
-    };
-    AppComponent.prototype.getAnimals = function (family, animals) {
-        var arrayOfAnimals = [];
-        animals.forEach(function (a) {
+    AppComponent.prototype.getAnimals = function (family) {
+        console.log(family.name);
+        var members = [];
+        this.animals.forEach(function (a) {
             if (a.family === family) {
-                arrayOfAnimals.push(a);
-                console.log(a);
-                console.log(arrayOfAnimals);
+                members.push(a);
             }
         });
-        console.log(arrayOfAnimals);
-        this.animalsOfFamily = arrayOfAnimals;
+        this.animalsOfFamily = members;
+        console.log(members);
+        console.log(this.animalsOfFamily);
+    };
+    AppComponent.prototype.getAmount = function (family) {
+        var amount = 0;
+        this.animals.forEach(function (a) {
+            if (a.family === family) {
+                amount++;
+            }
+        });
+        return amount;
     };
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
+        changeDetection: core_2.ChangeDetectionStrategy.OnPush,
         selector: 'my-app',
-        template: "<h1>{{globalList}}</h1>\n            <ul class=\"animals\">\n                <li *ngFor=\"let family of families\"\n                [class.selected]=\"family === selectedFamily\"\n                (click)=\"onSelect(family)\">\n                    <span class=\"badge\">{{family.id}}</span> {{family.name}}\n                    eat {{family.diet}} and there are {{families.length}}\n                </li>\n            </ul>\n            <div *ngIf=\"selectedFamily\">\n                <h2>{{selectedFamily.name}}</h2>\n                <li *ngFor=\"let animal of animalsOfFamily\">\n                    <span class=\"badge\">{{animal.id}}</span> {{animal.name}}\n                </li>\n            </div>",
+        template: "\n          <h1 (click)=\"showGlobalList(true)\">{{globalList}}</h1>\n            <div *ngIf=\"globalOrSpecific === true\">\n            <ul class=\"animals\">\n                <li *ngFor=\"let family of families\">\n                    <span class=\"badge\">{{family.id}}</span> {{family.name}}\n                    eat {{family.diet}} and there are {{this.getAmount(family)}}\n                </li>\n            </ul>\n            </div>\n            <h1 (click)=\"showGlobalList(false)\">{{specificList}}</h1>\n            <div *ngIf=\"globalOrSpecific === false\">\n                <li *ngFor=\"let family of families\">\n                {{this.getAnimals(family)}}\n                <h2>{{family.name}}</h2>\n                    <ul class=\"animals\">\n                        <li *ngFor=\"let animal of animalsOfFamily\">\n                            <span class=\"badge\">{{animal.id}}</span> {{animal.name}} is {{animal.age}}\n                        </li>\n                    </ul>\n                </li>\n            </div>",
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,31 @@
 import { Component } from '@angular/core';
 import { GlobalFamily } from './global-family';
 import { Animal } from './animal';
+import {  Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'my-app',
-  template: `<h1>{{globalList}}</h1>
+  template: `
+          <h1 (click)="showGlobalList(true)">{{globalList}}</h1>
+            <div *ngIf="globalOrSpecific === true">
             <ul class="animals">
-                <li *ngFor="let family of families"
-                [class.selected]="family === selectedFamily"
-                (click)="onSelect(family)">
+                <li *ngFor="let family of families">
                     <span class="badge">{{family.id}}</span> {{family.name}}
-                    eat {{family.diet}} and there are {{families.length}}
+                    eat {{family.diet}} and there are {{this.getAmount(family)}}
                 </li>
             </ul>
-            <div *ngIf="selectedFamily">
-                <h2>{{selectedFamily.name}}</h2>
-                <li *ngFor="let animal of animalsOfFamily">
-                    <span class="badge">{{animal.id}}</span> {{animal.name}}
+            </div>
+            <h1 (click)="showGlobalList(false)">{{specificList}}</h1>
+            <div *ngIf="globalOrSpecific === false">
+                <li *ngFor="let family of families">
+                {{this.getAnimals(family)}}
+                <h2>{{family.name}}</h2>
+                    <ul class="animals">
+                        <li *ngFor="let animal of animalsOfFamily">
+                            <span class="badge">{{animal.id}}</span> {{animal.name}} is {{animal.age}}
+                        </li>
+                    </ul>
                 </li>
             </div>`,
 })
@@ -31,35 +40,41 @@ export class AppComponent  {
       {id: 3, name: 'Birds', diet: 'Seed', noise: 'Sing',
           ability: 'Sing'},
     ];
+    specificList = 'Specific List';
     animals: Animal[] = [
-      {id: '11', name: 'Bobby', family: this.families[0]},
-      {id: '12', name: 'Toby', family: this.families[1]},
-      {id: '13', name: 'Speaker', family: this.families[2]},
-      {id: '14', name: 'Zipper', family: this.families[0]},
-      {id: '15', name: 'Walker', family: this.families[0]},
+      {id: '11', name: 'Bobby', age: 6, family: this.families[0]},
+      {id: '12', name: 'Toby', age: 24, family: this.families[2]},
+      {id: '13', name: 'Speaker', age:2, family: this.families[2]},
+      {id: '14', name: 'Zipper', age: 11, family: this.families[1]},
+      {id: '15', name: 'Walker', age: 8, family: this.families[0]},
     ];
     selectedFamily: GlobalFamily;
     animalsOfFamily: Animal[];
 
-    onSelect(family: GlobalFamily): void {
-        this.selectedFamily = family;
-        this.getAnimals(family, this.animals);
+    showGlobalList(flag: boolean): void {
+        this.globalOrSpecific = flag;
     }
 
-    count(family: GlobalFamily): int {
-        return family.length;
-    }
-
-    getAnimals(family: GlobalFamily, animals: Animal[]): void {
-        var arrayOfAnimals = [];
-        animals.forEach(function(a) {
+    getAnimals(family: GlobalFamily): void {
+        console.log(family.name);
+        var members = [];
+        this.animals.forEach(function(a) {
             if (a.family === family) {
-                arrayOfAnimals.push(a);
-                console.log(a);
-                console.log(arrayOfAnimals);
+                members.push(a);
             }
         });
-        console.log(arrayOfAnimals);
-        this.animalsOfFamily = arrayOfAnimals;
+        this.animalsOfFamily = members;
+        console.log(members);
+        console.log(this.animalsOfFamily);
+    }
+
+    getAmount(family: GlobalFamily): int {
+        var amount = 0;
+        this.animals.forEach(function(a) {
+            if (a.family === family) {
+                amount++;
+            }
+        });
+        return amount;
     }
 }
